01a944e40bac938dfcc2de0416fe6aeb8c228038
Sasa Milic <sasa@ss1.ais.sandbox>
2017-12-23 15:34:34 -0500

    using semcor package in python instead of reading raw file

--- map_wordnet_wordsmyth.py
+++ map_wordnet_wordsmyth.py
@@ -1,86 +1,97 @@
+import csv
 import nltk
-from lxml import html
 import os
-import xml.etree.ElementTree as ET
-import urllib2
-from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-import wget
 import ssl
-from nltk.tokenize import RegexpTokenizer
+import uuid
+import urllib2
+import wget
+import xml.etree.ElementTree as ET
 #from nltk.stemmer import PorterStemmer
-from stemming.porter2 import stem
+from nltk.corpus import semcor
+from bs4 import BeautifulSoup
+from lxml import html
+from multiprocessing import Pool
+from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
+from nltk.tokenize import RegexpTokenizer
+from stemming.porter2 import stem
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 tokenizer = RegexpTokenizer(r'\w+') # no punctuation
 
-SEMCOR_DIR = '/p/cl/SemCor/semcor3.0/brown1/tagfiles'
-OUTDIR = '~/homonymy_polysemy/semcor_convert'
-
+NUM_PROCESSES = 16
+OUTDIR = './semcor_convert'
 STOPWORDS = frozenset(stopwords.words('english'))
 
 
 def main():
     # create new directory
-    #os.makedirs(OUTDIR)
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
+
+    pool = Pool(processes=NUM_PROCESSES)
+    pool.map(main2, os.listdir(SEMCOR_DIR))    
+    #main2('br-a02')
+
+def main2(filename):
     # csv fieldnames
-    fieldnames = ['word', 'lemma', 'pos', 'is_homonym', 'wn_synset', 'ws_meaning', 'context', 'id']
-    #writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-
-    for filename in os.listdir(SEMCOR_DIR):
-        #outfile = open(os.path.join(OUTDIR, filename + '.tsv'))
-        #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        #writer.writeheader()
-
-        filename = os.path.join(SEMCOR_DIR, filename)
-        parsed = html.fromstring(open(filename).read())
-        for x in parsed.getiterator():
-            '''
-            if x.tag == 'p':
-                p_curr = x.attrib['pnum']
-            elif x.tag == 's':
-                s_curr = x.attrib['snum']
-            elif x.tag == 'wf' and pos in ['NN', 'VB']:
-                row = {}
-                row['word'] = x.text.lower()
-                row['lemma'] = x.attrib['lemma']
-                row['pos'] = x.attrib['pos']
-                row['is_homonym'] = False #TODO
-                row['wn_synset'] = False #TODO
-                row['ws_meaning'] = False #TODO
-                row['context'] = False 
- 
-            # append to curr_sentence
-            if x.tag == 'wf':
-                sent_curr = False #TODO
-            '''
-            print(x.tag, x.attrib, x.text, x.tail)
-
-        outfile.close()
-
-def chunk_sentences(f):
-    """
-    Generator to read SemCor file in sentence chunks.
-    """
-    # SemCor files have incorrect xml format >:(
-    # therefore parsing with html parser
+    fieldnames = ['word', 'lemma', 'pos', 'is_homonym', 'wn_synset', 'ws_meaning', 'confidence', 'nsenses']
     
-    while True:
-        # skip lines before paragraph
-        line = f.readline()
-        while not line.startswith("<p pnum="):
-            line = f.readline()
-        #while line.strip() != "</s>"
+    print('Reading {}...'.format(filename))
+    outfile = open(os.path.join(OUTDIR, filename + '.tsv'), 'w')
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
+    writer.writeheader()
 
+    for i,sent in enumerate(semcor.tagged_sents(tag='both')):
 
-    parsed = html.fromstring(f.read())
-    for x in parsed.getiterator():
-        if x.tag == 'p':
-            p_curr = x.attrib['pnum']
-        elif x.tag == 's':
-            s_curr = x.attrib['snum']
+        if i % 100 == 0:
+            print('{} sentences read'.format(i))
+
+        for t in sent:
+            if type(t) == str:
+                # no lemma
+                continue
+            word = t.flatten()[0].lower()
+            pos = t.pos()[0][1]
+            if pos != 'NN' or not pos.startswith('V'):
+                # t is not a noun or verb
+                continue
+            lemma = t.label().name()
+            wn_synset = t.label().synset()
+            nsenses = len(wn.synsets(word))
+
+            def_bag = set([stem(w.lower()) for w in wn_synset.definition()])
+            urls = homonym_urls(lemma)
+            if urls == []:
+                row['is_homonym'] = False
+                #row['ws_meaning'] = 1
+                #row['confidence'] = 1
+            else:
+
+                if pos == 'N':
+                    pos_ = 'noun'
+                elif pos.startswith('V'):
+                    pos_ = 'verb'
+ 
+                meaning_bags = [meaning_bag(pos_, url) for url in urls]                
+                confidence = [] # list of overlap size between bags
+                for mb in meaning_bags:
+                    confidence.append(len(mb & def_bag))
+
+                row['is_homonym'] = True
+                row['ws_meaning'] = confidence.index(max(confidence))
+                row['confidence'] = '|'.join(str(x) for x in confidence)
+
+            row['word'] = word
+            row['lemma'] = lemma
+            row['pos'] = pos
+            row['wn_synset'] = wn_synset
+            row['nsenses'] = nsenses   
+       
+            writer.writerow(row) 
+
+    outfile.close()
 
 
 def wordsmyth_meaning_bags(word):
@@ -122,16 +133,6 @@ def wordsmyth_meaning_bags(word):
 
     return result
 
-def lexsn_to_synset(lexsn):
-    '''
-    Convert `lexsn` from SemCor to Synset name
-    e.g. '5:00:00:gross:00' -> 'overall.s.02.overall'
-    '''
-    lemma = lexsn.split('%')[0]
-    for ss in wn.synsets(lemma):
-        for l in ss.lemmas():
-            if l.key() == lexsn:
-                return ss
 
 def homonym_urls(word):
     '''
@@ -141,19 +142,20 @@ def homonym_urls(word):
     ***A word is considered a homonym if it has more than one definition on WordSmyth.
     '''
     url = "https://www.wordsmyth.net/?level=3&ent={}".format(word)
-    filename = wget.download(url)
+    filename = wget.download(url, out='{}.html'.format(uuid.uuid4().hex))
     page = open(filename).read()
     soup = BeautifulSoup(page, 'html.parser')
     os.remove(filename)
-    if len(soup.find_all('div', class_ = "more")) == 0:
-        return False
+    #if len(soup.find_all('div', class_ = "more")) == 0:
+    #    return []
     results = []
+    if not soup.find('div', class_ = "wordlist"):
+        return []
     tbody = soup.find('div', class_ = "wordlist").table.tbody
     for td in tbody.find_all('td'):
         if td.find('a') and td.find('a').find('sup'):
             results.append(td.find('a').get('href'))
     return results
-
 
 def meaning_bag(pos, url):
     '''
@@ -165,7 +167,7 @@ def meaning_bag(pos, url):
     '''
 
     # download page to file
-    filename = wget.download(url)
+    filename = wget.download(url, out='{}.html'.format(uuid.uuid4().hex))
     page = open(filename).read()
     soup = BeautifulSoup(page, 'html.parser')
     # delete file !!!
@@ -177,8 +179,12 @@ def meaning_bag(pos, url):
     for tr in maintable.tbody.findChildren():
 
         if tr.get("class") and tr.get("class")[0] == "postitle":
-            pos_ = tr.find("td", class_ = "data").a.text
-            if pos_ == pos:
+            if tr.find("td", class_ = "data").a:
+                pos_ = tr.find("td", class_ = "data").a.text
+            else:
+                pos_ = tr.find("td", class_ = "data").text    
+
+            if pos in pos_.split():
                 correct_pos = True
             else:
                 correct_pos = False
@@ -207,11 +213,5 @@ def meaning_bag(pos, url):
 
 if __name__ == "__main__":
     #main()
-    #get_wordsmyth_meaning("bank")
-    #get_wordsmyth_meaning("dog")
-    #print(test2('noun'))
-    #print(homonym_urls("dog"))
-    #print(homonym_urls("cat"))
-    for x in ['charge%1:04:03::', 'election%1:04:01::']:
-        print(lexsn_to_synset(x).definition())
-        print(homonym_urls(x.split('%')[0]))
+    main2('bigfile')
+
